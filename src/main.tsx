44713966949f571189f014dc028c5933cc1c04c8
Sera Choi <c.sera2511@gmail.com>
2024-11-10 20:50:45 +0900

    test: build Sentry in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import * as Sentry from "@sentry/react";
 
 const queryClient = new QueryClient();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
